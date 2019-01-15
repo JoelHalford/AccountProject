b004143eeae9b80d9e94ff70ec41569a01086846
Joel <topsupp@hotmail.com>
2019-01-15 14:10:10 +0000

    Added account and service, updated runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,5 +4,16 @@ public class Runner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world to the standard out");
+		
+		Account john = new Account("John", "Wayne", 1);
+		Account michael = new Account("Michael", "Jackson", 2);
+		
+		Service newService = new Service();
+		
+		newService.addAccount(john);
+		newService.addAccount(michael);
+		
+		newService.retrieveAccounts();
+		
 	}
 }
